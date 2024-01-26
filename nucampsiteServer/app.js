2078a5d0742599cddedd973d4cd888ad52deacbf
Christine Wingfield <wingtek2020@gmail.com>
2024-01-26 13:24:23 -0500

    Express Generator

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
 
 var app = express();
 
@@ -18,6 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
